ed3aa89168ff05b78f9fdd80778983a0c565f6cf
liangding <liangding@Ctrip.com>
2017-05-31 16:35:32 +0800

    no message

---
+++ src/com/ding/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.ding.test;
+
+/**
+ * Created by liangding on 2017/5/31.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+     System.out.print("hello world");
+    }
+}
